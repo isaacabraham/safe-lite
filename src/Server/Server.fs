489c4504faedd895af63f3168a1bf5dad87e8508
Isaac Abraham <isaac.abraham@gmail.com>
2020-05-27 18:20:07 +0200

    Simplify server more

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -5,11 +5,8 @@ open Shared
 
 let webApp =
     router {
-        get "/api/init" (fun next ctx ->
-            task {
-                let counter = { Value = 42 }
-                return! json counter next ctx
-            }) }
+        get "/api/init" (json { Value = 42 })
+    }
 
 let app =
     application {
